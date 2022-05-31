fcb7f4b9b76a8ba5114cfd0d2b002023c15a744b
Prateek Asthana <paryanz@gmail.com>
2022-05-31 01:26:56 -0700

    removed redundant "description" from ChildVerifierSpec

--- src/test/scala/com/prateek/xmlcompare/verify/ChildVerifierSpec.scala
+++ src/test/scala/com/prateek/xmlcompare/verify/ChildVerifierSpec.scala
@@ -6,82 +6,80 @@ import org.scalatest.funspec.AnyFunSpec
 
 class ChildVerifierSpec extends AnyFunSpec {
 
-  describe("when expected and actual nodes count do not match") {
-    describe("when all the expected nodes match subset of actual nodes") {
-      it("should return Match") {
-        val en = <Node>
-          <Node1/>
-          <Node2/>
-          <Node5/>
-        </Node>
-        val an = <Node>
-          <Node0/>
-          <Node1/>
-          <Node2/>
-          <Node3/>
-          <Node4/>
-          <Node5/>
-        </Node>
-        val mv = new Verifier {
-          override def apply(exp: Node, act: Node)(using
-              ctx: Context
-          ): VerificationResult = {
-            if exp.equals(act) then Match
-            else NodeNotFound(exp.label)
-          }
+  describe("when all the expected nodes match subset of actual nodes") {
+    it("should return Match") {
+      val en = <Node>
+        <Node1/>
+        <Node2/>
+        <Node5/>
+      </Node>
+      val an = <Node>
+        <Node0/>
+        <Node1/>
+        <Node2/>
+        <Node3/>
+        <Node4/>
+        <Node5/>
+      </Node>
+      val mv = new Verifier {
+        override def apply(exp: Node, act: Node)(using
+            ctx: Context
+        ): VerificationResult = {
+          if exp.equals(act) then Match
+          else NodeNotFound(exp.label)
         }
-        val result = ChildVerifier(mv)(en, an)(using Context())
-        assertResult(Match)(result)
       }
+      val result = ChildVerifier(mv)(en, an)(using Context())
+      assertResult(Match)(result)
     }
+  }
 
-    describe("when the first expected node is missing") {
-      it("should return NodeNotFound with the missing first node label") {
-        val en = <Node>
-          <Node1/>
-          <Node2/>
-          <Node5/>
-        </Node>
-        val an = <Node>
-          <Node0/>
-          <Node2/>
-          <Node5/>
-        </Node>
-        val mv = new Verifier {
-          override def apply(exp: Node, act: Node)(using
-              ctx: Context
-          ): VerificationResult = {
-            if exp.equals(act) then Match
-            else NodeNotFound(exp.label)
-          }
+  describe("when the first expected node is missing") {
+    it("should return NodeNotFound with the missing first node label") {
+      val en = <Node>
+        <Node1/>
+        <Node2/>
+        <Node5/>
+      </Node>
+      val an = <Node>
+        <Node0/>
+        <Node2/>
+        <Node5/>
+      </Node>
+      val mv = new Verifier {
+        override def apply(exp: Node, act: Node)(using
+            ctx: Context
+        ): VerificationResult = {
+          if exp.equals(act) then Match
+          else NodeNotFound(exp.label)
         }
-        val result = ChildVerifier(mv)(en, an)(using Context())
-        assertResult(NodeNotFound("Node1"))(result)
       }
+      val result = ChildVerifier(mv)(en, an)(using Context())
+      assertResult(NodeNotFound("Node1"))(result)
     }
+  }
 
-    describe("when the last expected node is missing") {
-      it("should return NodeNotFound with the missing last node label") {
-        val en = <Node>
-          <Node1/>
-          <Node2/>
-          <Node5/>
-        </Node>
-        val an = <Node>
-          <Node1/>
-          <Node2/>
-        </Node>
-        val mv = new Verifier {
-          override def apply(exp: Node, act: Node)(using
-              ctx: Context
-          ): VerificationResult = {
-            if exp.equals(act) then Match
-            else NodeNotFound(exp.label)
-          }
+  describe("when the last expected node is missing") {
+    it("should return NodeNotFound with the missing last node label") {
+      val en = <Node>
+        <Node1/>
+        <Node2/>
+        <Node5/>
+      </Node>
+      val an = <Node>
+        <Node1/>
+        <Node2/>
+      </Node>
+      val mv = new Verifier {
+        override def apply(exp: Node, act: Node)(using
+            ctx: Context
+        ): VerificationResult = {
+          if exp.equals(act) then Match
+          else NodeNotFound(exp.label)
         }
-        val result = ChildVerifier(mv)(en, an)(using Context())
-        assertResult(NodeNotFound("Node5"))(result)
       }
+      val result = ChildVerifier(mv)(en, an)(using Context())
+      assertResult(NodeNotFound("Node5"))(result)
     }
   }
 }
