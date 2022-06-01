829f2d3fb41613906cd169fd68d423b05c1409c5
Prateek Asthana <paryanz@gmail.com>
2022-06-01 13:43:57 -0700

    simplified version of DiscoverResponse parsing working

--- src/main/scala/com/prateek/xmlcompare/yaml/ComparingCriteriaYamlReader.scala
+++ src/main/scala/com/prateek/xmlcompare/yaml/ComparingCriteriaYamlReader.scala
@@ -12,28 +12,35 @@ object ComparingCriteriaYamlReader extends App {
   val config =
     getClass.getClassLoader.getResourceAsStream("yaml/criteria-config.yaml")
   val json: Json = yaml.parser.parse(new InputStreamReader(config)).get
-
   println(s"json $json")
-  private val nrc: NodeVerifiersConfig = json.as[NodeVerifiersConfig].get
-  println(s"nrc: ${nrc.nodeToVerifiersMap}")
 
-  case class NodeVerifiersConfig(nodeVerifiers: List[JsonObject]) {
-    val nodeToVerifiersMap: Map[String, List[String]] = {
-      val nodeJsonTuples: Seq[(String, Json)] = nodeVerifiers
-        .flatMap(f => f.toMap)
-      val nodeToJsonMap: Map[String, Json] = nodeJsonTuples.toMap
-      nodeToJsonMap
-        .map({ case (k, v) =>
-          val x = v.as[List[String]].get
-          (k, x)
-        })
+  implicit val discoverResponseDecoder: Decoder[DiscoverResponse] =
+    (c: HCursor) => {
+      /*
+            import io.circe.Decoder.Result
+            val nodeVerifiersJson: Result[List[JsonObject]] =
+              c.value.as[List[JsonObject]]
+            val nodeJsonTuples: Seq[(String, Json)] =
+              nodeVerifiersJson.get.flatMap(f => f.toMap)
+            val nvs: List[NodeVerifier] = nodeJsonTuples
+              .map({ case (k, v) =>
+                val verifiers = v.as[List[String]].get
+                NodeVerifier(k, verifiers)
+              })
+              .toList
+       */
+      Right(DiscoverResponse(List("nvs")))
     }
-  }
+  val dr: DiscoverResponse = json.as[DiscoverResponse].get
 
-  object NodeVerifiersConfig {
-    implicit val nodeVerifiersConfigDecoder: Decoder[NodeVerifiersConfig] =
-      deriveDecoder[NodeVerifiersConfig]
-  }
+  /*
+  case class NodeVerifier(n: String, v: List[String])
+
+  case class DiscoverResponse(discoverResponse: List[NodeVerifier])
+   */
+  case class DiscoverResponse(discoverResponse: List[String])
+
+  println(s"$dr")
 
   extension [A](e: Either[Error, A]) {
     def get: A = {
