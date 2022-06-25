496b5dd2984972e4844d998cd807616cbe7e685f
Prateek Asthana <paryanz@gmail.com>
2022-06-25 14:22:19 -0700

    add VerificationPredicate.scala

--- src/main/scala/com/prateek/xmlcompare/verify/VerificationPredicate.scala
+++ src/main/scala/com/prateek/xmlcompare/verify/VerificationPredicate.scala
@@ -19,6 +19,19 @@ object VerificationPredicate:
 
 class RegexVerificationPredicate(vc: VerificationConfig) extends VerificationPredicate:
   override def apply(msg: Message, vid: VerifierId, xp: XPath): Boolean =
+    // checks if config provided xpath matches the xml file xpath
+    def matches(cxp: XPath): Boolean =
+      val regex = cxp.r
+      regex.matches(xp)
+    end matches
+
     msg match
-      case DiscoverResponse => ???
-      case _                => ???
+      case DiscoverResponse =>
+        val matchingVerifierIds = vc.discoverResponseConfig.collect({
+          case (configXpath: XPath, vids) if matches(configXpath) => vids
+        })
+        val minMatchingVerifiers = matchingVerifierIds.minBy(_.size)
+        minMatchingVerifiers.contains(vid)
+      case _ => ???
+  end apply
+end RegexVerificationPredicate
