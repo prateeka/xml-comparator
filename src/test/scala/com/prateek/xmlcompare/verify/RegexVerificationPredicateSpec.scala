8c4bd352a8ba947cc7688037f79fec11633ed6a0
Prateek Asthana <paryanz@gmail.com>
2022-06-25 16:19:15 -0700

    reformat RegexVerificationPredicateSpec to use "raw" instead of triple quotes

--- src/test/scala/com/prateek/xmlcompare/verify/RegexVerificationPredicateSpec.scala
+++ src/test/scala/com/prateek/xmlcompare/verify/RegexVerificationPredicateSpec.scala
@@ -9,56 +9,57 @@ import com.prateek.xmlcompare.read.DiscoverResponse
 import com.prateek.xmlcompare.verify.Identification.VerifierId
 
 class RegexVerificationPredicateSpec extends AnyFunSpec:
-  describe("node label test") {
+  describe("test node label") {
     describe("when xpath exact match is NOT found and only default matches") {
       it("is true as the verifier is in default verifiers list") {
-        val mvc1 = new MVC("""n1\\n2\\n3""", Set("labeltext", "attribute"))
+        val mvc1 = new MVC(raw"e1\\e2\\e3", Set("labeltext", "attribute"))
         val mvc2 = new MVC(".+", Set("labeltext", "child"))
         val vc = VerificationConfig(Seq(mvc1, mvc2))
         val bool = RegexVerificationPredicate(vc).apply(
           DiscoverResponse,
           "labeltext",
-          "n1\\n2"
+          raw"e1\e2"
         )
         bool shouldBe true
       }
 
       it("is false as the verifier is NOT in default verifiers list") {
-        val mvc1 = new MVC("""n1\\n2\\n3""", Set("labeltext", "attribute"))
+        val mvc1 = new MVC(raw"e1\\e2\\e3", Set("labeltext", "attribute"))
         val mvc2 = new MVC(".+", Set("labeltext", "child"))
         val vc = VerificationConfig(Seq(mvc1, mvc2))
         val bool = RegexVerificationPredicate(vc).apply(
           DiscoverResponse,
           "attribute",
-          "n1\\n2"
+          raw"e1\e2"
         )
         bool shouldBe false
       }
     }
 
-    describe("when xpath exact match is found") {
+    describe(
+      "when xpath exact match is found in mulitple points and the smallest verifier list is chosen"
+    ) {
       it("is true as the verifier is in default verifiers list") {
         val mvc1 =
-          new MVC("""e1\\e2""", Set("labeltext", "attribute"))
+          new MVC(raw"e1\\e2", Set("attribute"))
         val mvc2 = new MVC(".+", Set("labeltext", "child"))
         val vc = VerificationConfig(Seq(mvc1, mvc2))
         val bool = RegexVerificationPredicate(vc)(
           DiscoverResponse,
           "attribute",
-          "e1\\e2"
+          raw"e1\e2"
         )
         bool shouldBe true
       }
 
       it("is false as the verifier is NOT in default verifiers list") {
-        val mvc1 =
-          new MVC("""e1\\e2""", Set("labeltext", "attribute"))
-        val mvc2 = new MVC(".+", Set("labeltext", "attribute", "child"))
+        val mvc1 = new MVC(raw"e1\\e2", Set("attribute"))
+        val mvc2 = new MVC(".+", Set("labeltext", "child"))
         val vc = VerificationConfig(Seq(mvc1, mvc2))
         val bool = RegexVerificationPredicate(vc)(
           DiscoverResponse,
           "child",
-          "e1\\e2"
+          raw"e1\e2"
         )
         bool shouldBe false
       }
