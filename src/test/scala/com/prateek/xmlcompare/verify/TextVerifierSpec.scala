b3c7bbbce026f5539fd85384c290c300c6a699e6
Prateek Asthana <paryanz@gmail.com>
2022-06-26 17:59:37 -0700

    rename LabelTextVerifierSpec.scala to TextVerifierSpec.scala and fixes it

--- src/test/scala/com/prateek/xmlcompare/verify/TextVerifierSpec.scala
+++ src/test/scala/com/prateek/xmlcompare/verify/TextVerifierSpec.scala
@@ -5,23 +5,23 @@ import org.scalatest.matchers.should.Matchers.*
 
 import com.prateek.xmlcompare.read.DiscoverResponse
 
-class LabelTextVerifierSpec extends AnyFunSpec {
+class TextVerifierSpec extends AnyFunSpec {
 
   describe("comparing two matching nodes") {
     it("should return Match") {
       val exp = <Node>"test for matching node</Node>
       val act = <Node>"test for matching node</Node>
       val ctx: VerificationContext = VerificationContext(DiscoverResponse).append(exp)
-      assertResult(Match)(LabelTextVerifier(exp, act)(using ctx))
+      assertResult(Match)(TextVerifier(exp.child.head, act.child.head)(using ctx))
     }
   }
 
   describe("nodes mismatch") {
-    it("should return NodeNotFound with missing node") {
+    it("should return Match as verifier will come across Elem and not Text") {
       val exp = <Node1>"test for non-matching node</Node1>
       val act = <Node2>"test for non-matching node</Node2>
       val ctx: VerificationContext = VerificationContext(DiscoverResponse)
-      LabelTextVerifier(exp, act)(using ctx) shouldBe NodeNotFound("Node1")
+      TextVerifier(exp, act)(using ctx) shouldBe Match
     }
   }
 
@@ -30,7 +30,7 @@ class LabelTextVerifierSpec extends AnyFunSpec {
       val exp = <Node1>text1</Node1>
       val act = <Node1>text2</Node1>
       val ctx: VerificationContext = VerificationContext(DiscoverResponse).append(exp)
-      LabelTextVerifier(exp.child.head, act.child.head)(using ctx) shouldBe NodeTextNotFound(
+      TextVerifier(exp.child.head, act.child.head)(using ctx) shouldBe NodeTextNotFound(
         "Node1\\%TEXT"
       )
     }
