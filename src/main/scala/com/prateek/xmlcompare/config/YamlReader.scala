827efe4b308a2a576e850b1e3eafe1d876161df0
Prateek Asthana <paryanz@gmail.com>
2022-07-08 12:56:13 -0700

    create case class YamlReader from object YamlReader

--- src/main/scala/com/prateek/xmlcompare/config/YamlReader.scala
+++ src/main/scala/com/prateek/xmlcompare/config/YamlReader.scala
@@ -1,47 +1,62 @@
 package com.prateek.xmlcompare.config
 
-import java.io.InputStreamReader
+import scala.io.Source
+
+import java.io.{File, FileInputStream, InputStreamReader}
+import java.net.URL
 
 import cats.syntax.either.*
 
 import io.circe.*
+import io.circe.Decoder.Result
 import io.circe.generic.semiauto.*
 import io.circe.yaml.parser.parse
-import io.circe.Decoder.Result
+
+import com.prateek.xmlcompare.Main.getClass
 
 trait VerificationConfigReader:
-  def read(): VerificationConfig
+  def read(config: File): VerificationConfig
+end VerificationConfigReader
 
-//case class YamlReader() extends App:
+extension [A](e: Either[Error, A])
+  def get: A = e match
+    case Left(error)  => throw error
+    case Right(value) => value
+end extension
+
+given verificationConfigDecoder: Decoder[VerificationConfig] = (c: HCursor) =>
+  // each JsonObject corresponds to MessageVerificationConfig
+  val v1: Seq[JsonObject] =
+    val v11 = c.downField("discoverResponse")
+    v11.as[List[JsonObject]].get
+  val v2: Seq[(XPathRegex, Set[VerifierId])] =
+    val v22: Seq[(XPathRegex, Json)] = v1.flatMap(_.toList)
+    v22.map({ case (xpr, json) =>
+      val vids = json.as[Set[VerifierId]].get
+      (xpr, vids)
+    })
+  Right(VerificationConfig(v2))
+end verificationConfigDecoder
+
+given verifierIdsDecoder: Decoder[Set[VerifierId]] = (c: HCursor) =>
+  val stringVerifierIds = c.as[Set[String]].get
+  val vids = stringVerifierIds.map(VerifierId.valueOf)
+  Right(vids)
+end verifierIdsDecoder
+
+case class YamlReader() extends VerificationConfigReader:
+  private val logger = com.typesafe.scalalogging.Logger(getClass)
+
+  def read(config: File): VerificationConfig =
+    val inputStream = new FileInputStream(config)
+    val json: Json = parse(new InputStreamReader(inputStream)).get
+    logger.debug(s"json: $json")
+    val vc: VerificationConfig = json.as[VerificationConfig].get
+    logger.debug(s"vc is $vc")
+    vc
+end YamlReader
+
 object YamlReader extends App:
-  val inputStream = getClass.getClassLoader.getResourceAsStream("yaml/criteria-config.yaml")
-  val json: Json = parse(new InputStreamReader(inputStream)).get
-  println(s"json: $json")
-  val vc: VerificationConfig = json.as[VerificationConfig].get
-  println(s"vc is $vc")
-
-  given verificationConfigDecoder: Decoder[VerificationConfig] = (c: HCursor) =>
-    // each JsonObject corresponds to MessageVerificationConfig
-    val v1: Seq[JsonObject] =
-      val v11 = c.downField("discoverResponse")
-      v11.as[List[JsonObject]].get
-    val v2: Seq[(XPathRegex, Set[VerifierId])] =
-      val v22: Seq[(XPathRegex, Json)] = v1.flatMap(_.toList)
-      v22.map({ case (xpr, json) =>
-        val vids = json.as[Set[VerifierId]].get
-        (xpr, vids)
-      })
-    Right(VerificationConfig(v2))
-
-  given verifierIdsDecoder: Decoder[Set[VerifierId]] = (c: HCursor) =>
-    val stringVerifierIds = c.as[Set[String]].get
-    val vids = stringVerifierIds.map(VerifierId.valueOf)
-    Right(vids)
-
-  //  override def read(): VerificationConfig = ???
-  extension [A](e: Either[Error, A]) {
-    def get: A = e match
-      case Left(error)  => throw error
-      case Right(value) => value
-  }
+  val url: URL = getClass.getClassLoader.getResource("yaml/criteria-config.yaml")
+  YamlReader().read(new File(url.toURI))
 end YamlReader
