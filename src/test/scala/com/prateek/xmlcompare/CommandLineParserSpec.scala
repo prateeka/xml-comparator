de74b330b59185064b7ce1e3513b5a9aa8ae8a7d
Prateek Asthana <paryanz@gmail.com>
2022-07-24 15:03:04 -0700

    fix CommandLineParserSpec failing test

--- src/test/scala/com/prateek/xmlcompare/CommandLineParserSpec.scala
+++ src/test/scala/com/prateek/xmlcompare/CommandLineParserSpec.scala
@@ -6,18 +6,20 @@ import org.scalatest.funspec.AnyFunSpec
 class CommandLineParserSpec extends AnyFunSpec {
   describe("command line arguments are correctly read") {
     it("-a and -e are read") {
-      val a2 = "file2"
       val a1 = "file1"
-      val args = s"-a $a2 -e $a1".split("\\s+")
+      val a2 = "file2"
+      val a3 = "file3"
+      val args = s"-a $a2 -e $a1 -c $a3".split("\\s+")
       val clp = CommandLineParser[String](args)
       assertResult(clp.expected())(a1)
       assertResult(clp.actual())(a2)
     }
 
     it("--actual and --expected are read") {
-      val a2 = "file2"
       val a1 = "file1"
-      val args = s"--actual $a2 --expected $a1".split("\\s+")
+      val a2 = "file2"
+      val a3 = "file3"
+      val args = s"--actual $a2 --expected $a1 --config $a3".split("\\s+")
       val clp = CommandLineParser[String](args)
       assertResult(clp.expected())(a1)
       assertResult(clp.actual())(a2)
