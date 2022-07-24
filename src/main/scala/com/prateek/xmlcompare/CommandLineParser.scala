6f291a2e758e9df0174885d49cb3bf65975f85c3
Prateek Asthana <paryanz@gmail.com>
2022-07-24 14:35:01 -0700

    update CommandLineParser to parse CLI config as a file

--- src/main/scala/com/prateek/xmlcompare/CommandLineParser.scala
+++ src/main/scala/com/prateek/xmlcompare/CommandLineParser.scala
@@ -6,5 +6,6 @@ class CommandLineParser[T](arguments: Seq[String])(using ValueConverter[T])
     extends ScallopConf(arguments) {
   val expected: ScallopOption[T] = opt[T](required = true)
   val actual: ScallopOption[T] = opt[T](required = true)
+  val config: ScallopOption[T] = opt[T](required = true)
   verify()
 }
